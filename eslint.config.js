a86e0a85fed98b5a001c01414ac15709db030548
Patrick Xiao <pdxiao2@illinois.edu>
2026-09-24 13:26:42 -0700

    build: check and compile each pull request

--- eslint.config.js
+++ eslint.config.js
@@ -4,7 +4,7 @@ import tseslint from "typescript-eslint";
 
 export default tseslint.config(
   {
-    ignores: ["**/coverage/**"],
+    ignores: ["**/coverage/**", "**/dist/**"],
   },
   eslint.configs.recommended,
   ...tseslint.configs.strictTypeChecked,
